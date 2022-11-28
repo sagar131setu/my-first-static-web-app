120d35a4fdac55243f3f38da096e1379e4262480
Sagar Setu <Sagar.Setu@neudesic.com>
2022-11-28 11:38:09 +0530

    updated index.html and index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -4,3 +4,15 @@ module.exports = async function (context, req) {
         body: { text: "Hello from API" }
     };
 }
+
+module.exports = async function (context, req) {
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+
+    context.res = {
+        body: {
+            clientPrincipal: JSON.parse(decoded),
+        },
+    };
+};
